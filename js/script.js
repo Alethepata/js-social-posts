ccaff838354eab942d80e35469d38983e7426050
Alethepata <alessia.patane@gmail.com>
2023-09-28 18:59:02 +0200

    [edit] likeBtn

--- js/script.js
+++ js/script.js
@@ -59,46 +59,91 @@ const posts = [
     }
 ];
 
-posts.forEach((post) => {
+
+
+posts.forEach( post => container.innerHTML += template(post) )
+
+function template(post){
 
     const id = post.id;
     const content = post.content;
     const media = post.media;
     const author = post.author.name;
     const image = post.author.image;
-    const likes = post.likes;
+    let likes = post.likes;
     const created = post.created;
-   
-container.innerHTML += 
-`<div class="post">
- <div class="post__header">
-     <div class="post-meta">                    
-         <div class="post-meta__icon">
-             <img class="profile-pic" src="${image}" alt="${author}">                    
-         </div>
-         <div class="post-meta__data">
-             <div class="post-meta__author">${author}</div>
-             <div class="post-meta__time">${created}</div>
-         </div>                    
-     </div>
- </div>
- <div class="post__text">${content}</div>
- <div class="post__image">
-     <img src="${media}" alt="">
- </div>
- <div class="post__footer">
-     <div class="likes js-likes">
-         <div class="likes__cta">
-             <a class="like-button  js-like-button" href="#" data-postid="${id}">
-                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                 <span class="like-button__label">Mi Piace</span>
-             </a>
-         </div>
-         <div class="likes__counter">
-             Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
-         </div>
-     </div> 
- </div>            
-</div>`
+    return`
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                       <img class="profile-pic" src="${image}" alt="${author}">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${author}</div>
+                        <div class="post-meta__time">${created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${content}</div>
+            <div class="post__image">
+                <img src="${media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+                    </div> 
+            </div>            
+        </div>
+    
+    `
+    
+}
+
+
+const likeBtn = document.querySelectorAll('.js-like-button');
+const counter = document.querySelectorAll('.js-likes-counter');
+let mylike=[];
+
+
+likeBtn.forEach((btn,index) =>{
+    btn._id = posts[index].id;
+    btn._index = index;
+
+    btn.addEventListener('click', function(event){
+
+    event.preventDefault();
+    this.classList.toggle('like-button--liked');
+
+    const postLiked = posts.find( post => btn._id === post.id);
+
+    if(mylike.includes(this._id)){
+        mylike = mylike.filter( likeId => likeId !==  this._id);
+        postLiked.likes--; 
+    }else{
+        postLiked.likes++;
+        mylike.push(this._id);
+
+    }
+    counter[this._index].innerText = postLiked.likes;
+    
+  
+    });
+
+
+
 })
+
+
+
+
+
+
 
